d2e024d449b8648cee9cc3ad3d08b565645d54f7
Robert Hallberg <Robert.Hallberg@noaa.gov>
2016-06-30 18:16:24 -0400

    Fixed some SIS2 license statements
      Several of the SIS2 files had GPL license statements identifying them as
    either being a part of MOM or a part of FMS. These have now been corrected to
    identify them as a part of SIS2.  Only comments were changed.

--- SIS_tracer_flow_control.F90
+++ SIS_tracer_flow_control.F90
@@ -1,14 +1,14 @@
 module SIS_tracer_flow_control
     !***********************************************************************
     !*                   GNU General Public License                        *
-    !* This file is a part of MOM.                                         *
+    !* This file is a part of SIS2.                                        *
     !*                                                                     *
-    !* MOM is free software; you can redistribute it and/or modify it and  *
+    !* SIS2 is free software; you can redistribute it and/or modify it and *
     !* are expected to follow the terms of the GNU General Public License  *
     !* as published by the Free Software Foundation; either version 2 of   *
     !* the License, or (at your option) any later version.                 *
     !*                                                                     *
-    !* MOM is distributed in the hope that it will be useful, but WITHOUT  *
+    !* SIS2 is distributed in the hope that it will be useful, but WITHOUT *
     !* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *
     !* or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public    *
     !* License for more details.                                           *
--- ice_utils.F90
+++ ice_utils.F90
@@ -1,27 +1,22 @@
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!                                                                   !!
-!!                   GNU General Public License                      !!
-!!                                                                   !!
-!! This file is part of the Flexible Modeling System (FMS).          !!
-!!                                                                   !!
-!! FMS is free software; you can redistribute it and/or modify       !!
-!! it and are expected to follow the terms of the GNU General Public !!
-!! License as published by the Free Software Foundation.             !!
-!!                                                                   !!
-!! FMS is distributed in the hope that it will be useful,            !!
-!! but WITHOUT ANY WARRANTY; without even the implied warranty of    !!
-!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     !!
-!! GNU General Public License for more details.                      !!
-!!                                                                   !!
-!! You should have received a copy of the GNU General Public License !!
-!! along with FMS; if not, write to:                                 !!
-!!          Free Software Foundation, Inc.                           !!
-!!          59 Temple Place, Suite 330                               !!
-!!          Boston, MA  02111-1307  USA                              !!
-!! or see:                                                           !!
-!!          http://www.gnu.org/licenses/gpl.txt                      !!
-!!                                                                   !!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!***********************************************************************
+!*                   GNU General Public License                        *
+!* This file is a part of SIS2.                                        *
+!*                                                                     *
+!* SIS2 is free software; you can redistribute it and/or modify it and *
+!* are expected to follow the terms of the GNU General Public License  *
+!* as published by the Free Software Foundation; either version 2 of   *
+!* the License, or (at your option) any later version.                 *
+!*                                                                     *
+!* SIS2 is distributed in the hope that it will be useful, but WITHOUT *
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *
+!* or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public    *
+!* License for more details.                                           *
+!*                                                                     *
+!* For the full text of the GNU General Public License,                *
+!* write to: Free Software Foundation, Inc.,                           *
+!*           675 Mass Ave, Cambridge, MA 02139, USA.                   *
+!* or see:   http://www.gnu.org/licenses/gpl.html                      *
+!***********************************************************************
 
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~!
 ! This module contains convenient utilities for use by the SIS2 sea ice model. !
