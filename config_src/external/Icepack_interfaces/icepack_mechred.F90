8030d7314ffa6ad7dd06a6011525e33b259f7c1d
Matthew Harrison <Matthew.Harrison@noaa.gov>
2021-06-15 11:40:15 -0400

    Added some documentation for Icepack ridging interface

--- config_src/external/Icepack_interfaces/icepack_mechred.F90
+++ config_src/external/Icepack_interfaces/icepack_mechred.F90
@@ -9,6 +9,9 @@
       public :: ridge_ice
       contains
 
+!-----------------------------------------------------------------------
+!> Interface for updating the sea-ice state due to ice ridging processes
+!! using Icepack.
       subroutine ridge_ice (dt,          ndtd,       &
                             ncat,        n_aero,     &
                             nilyr,       nslyr,      &
@@ -35,154 +38,82 @@
                             closing_flag,closing )
 
       integer (kind=int_kind), intent(in) :: &
-         ndtd       , & ! number of dynamics subcycles
-         ncat  , & ! number of thickness categories
-         nilyr , & ! number of ice layers
-         nslyr , & ! number of snow layers
-         n_aero, & ! number of aerosol tracers
-         ntrcr     ! number of tracers in use
+         ndtd       , & !< Thenumber of dynamics subcycles  [nondim]
+         ncat  , & !< The number of thickness categories [nondim]
+         nilyr , & !< The number of ice layers [nondim]
+         nslyr , & !< The number of snow layers [nondim]
+         n_aero, & !< The number of aerosol tracers [nondim]
+         ntrcr     !< The number of tracers in use [nondim]
 
       real (kind=dbl_kind), intent(in) :: &
-         mu_rdg , & ! gives e-folding scale of ridged ice (m^.5)
-         dt             ! time step
+         mu_rdg , & !< The e-folding scale of ridged ice [Z^0.5 ~> m^0.5]
+         dt         !< The time step over which ridging occurs [T ~> s]
 
       real (kind=dbl_kind), dimension(0:ncat), intent(inout) :: &
-         hin_max   ! category limits (m)
+         hin_max   !< category limits [Z ~> m]
 
       real (kind=dbl_kind), intent(in) :: &
-         rdg_conv   , & ! normalized energy dissipation due to convergence (1/s)
-         rdg_shear      ! normalized energy dissipation due to shear (1/s)
+         rdg_conv   , & !< Normalized energy dissipation due to convergence [T-1 ~> s-1]
+         rdg_shear      !< Normalized energy dissipation due to shear [T-1 ~> s-1]
 
       real (kind=dbl_kind), dimension (:), intent(inout) :: &
-         aicen      , & ! concentration of ice
-         vicen      , & ! volume per unit area of ice          (m)
-         vsnon          ! volume per unit area of snow         (m)
+         aicen      , & !< The concentration of ice [nondim]
+         vicen      , & !< The volume per unit area of ice [L ~> m]
+         vsnon          !< The volume per unit area of snow [L ~> m]
 
       real (kind=dbl_kind), dimension (:,:), intent(inout) :: &
-         trcrn          ! ice tracers
+         trcrn          !< Ice tracer concentrations [R ~> kg m-3]
 
       real (kind=dbl_kind), intent(inout) :: &
-         aice0          ! concentration of open water
+         aice0          !< The concentration of open water [nondim]
 
       integer (kind=int_kind), dimension (:), intent(in) :: &
-         trcr_depend, & ! = 0 for aicen tracers, 1 for vicen, 2 for vsnon
-         n_trcr_strata  ! number of underlying tracer layers
+         trcr_depend, & !<  = 0 for aicen tracers, 1 for vicen, 2 for vsnon [nondim]
+         n_trcr_strata  !< The number of underlying tracer layers [nondim]
 
       real (kind=dbl_kind), dimension (:,:), intent(in) :: &
-         trcr_base      ! = 0 or 1 depending on tracer dependency
-                        ! argument 2:  (1) aice, (2) vice, (3) vsno
+         trcr_base      !< = 0 or 1 depending on tracer dependency [nondim]
+                        !! argument 2:  (1) aice, (2) vice, (3) vsno [nondim]
 
       integer (kind=int_kind), dimension (:,:), intent(in) :: &
-         nt_strata      ! indices of underlying tracer layers
+         nt_strata      !< Tndices of underlying tracer layers [nondim]
 
       integer (kind=int_kind), intent(in) :: &
-         krdg_partic, & ! selects participation function
-         krdg_redist    ! selects redistribution function
+         krdg_partic, & !< Selects participation function [nondim]
+         krdg_redist    !< Selects redistribution function [nondim]
 
       logical (kind=log_kind), intent(in) :: &
-         closing_flag, &! flag if closing is valid
-         tr_brine       ! if .true., brine height differs from ice thickness
+         closing_flag, &!< flag if closing is valid [nondim]
+         tr_brine       !< if .true., brine height differs from ice thickness [nondim]
 
       ! optional history fields
       real (kind=dbl_kind), intent(inout), optional :: &
-         dardg1dt   , & ! rate of fractional area loss by ridging ice (1/s)
-         dardg2dt   , & ! rate of fractional area gain by new ridges (1/s)
-         dvirdgdt   , & ! rate of ice volume ridged (m/s)
-         opening    , & ! rate of opening due to divergence/shear (1/s)
-         closing    , & ! rate of closing due to divergence/shear (1/s)
-         fpond      , & ! fresh water flux to ponds (kg/m^2/s)
-         fresh      , & ! fresh water flux to ocean (kg/m^2/s)
-         fhocn          ! net heat flux to ocean (W/m^2)
+         dardg1dt   , & !< The rate of fractional area loss by ridging ice [T-1 ~> s-1]
+         dardg2dt   , & !< The rate of fractional area gain by new ridges [T-1 ~> s-1]
+         dvirdgdt   , & !< The rate of ice volume ridged [L T-1 ~> m s-1]
+         opening    , & !< The rate of opening due to divergence/shear [T-1 ~> s-1]
+         closing    , & !< The rate of closing due to divergence/shear [T-1 ~> s-1]
+         fpond      , & !< The fresh water flux to ponds [R L T-1 ~> kg m2 s-1]
+         fresh      , & !< The fresh water flux to ocean [R L T-1 ~> kg m2 s-1]
+         fhocn          !< The net heat flux to ocean [W m-2]
 
       real (kind=dbl_kind), dimension(:), intent(inout), optional :: &
-         dardg1ndt  , & ! rate of fractional area loss by ridging ice (1/s)
-         dardg2ndt  , & ! rate of fractional area gain by new ridges (1/s)
-         dvirdgndt  , & ! rate of ice volume ridged (m/s)
-         aparticn   , & ! participation function
-         krdgn      , & ! mean ridge thickness/thickness of ridging ice
-         araftn     , & ! rafting ice area
-         vraftn     , & ! rafting ice volume
-         aredistn   , & ! redistribution function: fraction of new ridge area
-         vredistn       ! redistribution function: fraction of new ridge volume
+         dardg1ndt  , & !< The rate of fractional area loss by ridging ice [T-1 ~> s-1]
+         dardg2ndt  , & !< The rate of fractional area gain by new ridges [T-1 ~> s-1]
+         dvirdgndt  , & !< The rate of ice volume ridged [L*T-1 ~> m s-1]
+         aparticn   , & !< The participation function [nondim]
+         krdgn      , & !< The mean ridge thickness/thickness of ridging ice [Z ~> m]
+         araftn     , & !< The rafting ice area [L2 ~> m2]
+         vraftn     , & !< The rafting ice volume [L3 ~> m3]
+         aredistn   , & !< The redistribution function: fraction of new ridge area [nondim]
+         vredistn       !< The redistribution function: fraction of new ridge volume [nondim]
 
       real (kind=dbl_kind), dimension(:), intent(inout), optional :: &
-         faero_ocn      ! aerosol flux to ocean (kg/m^2/s)
+         faero_ocn      !< The aerosol flux to ocean [R L T-1 ~> kg m-2 s-1]
 
       real (kind=dbl_kind), dimension(:), intent(inout), optional :: &
-         fiso_ocn       ! isotope flux to ocean (kg/m^2/s)
+         fiso_ocn       !< isotope flux to ocean [R L T-1 ~> kg m-2 s-1]
 
-      ! local variables
-
-      real (kind=dbl_kind), dimension (ncat) :: &
-         eicen          ! energy of melting for each ice layer (J/m^2)
-
-      real (kind=dbl_kind), dimension (ncat) :: &
-         esnon, &       ! energy of melting for each snow layer (J/m^2)
-         vbrin, &       ! ice volume with defined by brine height (m)
-         sicen          ! Bulk salt in h ice (ppt*m)
-
-      real (kind=dbl_kind) :: &
-         asum       , & ! sum of ice and open water area
-         aksum      , & ! ratio of area removed to area ridged
-         msnow_mlt  , & ! mass of snow added to ocean (kg m-2)
-         esnow_mlt  , & ! energy needed to melt snow in ocean (J m-2)
-         mpond      , & ! mass of pond added to ocean (kg m-2)
-         closing_net, & ! net rate at which area is removed    (1/s)
-                        ! (ridging ice area - area of new ridges) / dt
-         divu_adv   , & ! divu as implied by transport scheme  (1/s)
-         opning     , & ! rate of opening due to divergence/shear
-                        ! opning is a local variable;
-                        ! opening is the history diagnostic variable
-         ardg1      , & ! fractional area loss by ridging ice
-         ardg2      , & ! fractional area gain by new ridges
-         virdg      , & ! ice volume ridged
-         aopen          ! area opening due to divergence/shear
-
-      real (kind=dbl_kind), dimension (n_aero) :: &
-         maero          ! aerosol mass added to ocean (kg m-2)
-
-      real (kind=dbl_kind), dimension (n_iso) :: &
-         miso          ! isotope mass added to ocean (kg m-2)
-
-      real (kind=dbl_kind), dimension (0:ncat) :: &
-         apartic        ! participation function; fraction of ridging
-                        ! and closing associated w/ category n
-
-      real (kind=dbl_kind), dimension (ncat) :: &
-         hrmin      , & ! minimum ridge thickness
-         hrmax      , & ! maximum ridge thickness (krdg_redist = 0)
-         hrexp      , & ! ridge e-folding thickness (krdg_redist = 1)
-         krdg       , & ! mean ridge thickness/thickness of ridging ice
-         ardg1n     , & ! area of ice ridged
-         ardg2n     , & ! area of new ridges
-         virdgn     , & ! ridging ice volume
-         mraftn         ! rafting ice mask
-
-      real (kind=dbl_kind) :: &
-         vice_init, vice_final, & ! ice volume summed over categories
-         vsno_init, vsno_final, & ! snow volume summed over categories
-         eice_init, eice_final, & ! ice energy summed over layers
-         vbri_init, vbri_final, & ! ice volume in fbri*vicen summed over categories
-         sice_init ,sice_final, & ! ice bulk salinity summed over categories
-         esno_init, esno_final    ! snow energy summed over layers
-
-      integer (kind=int_kind), parameter :: &
-         nitermax = 20  ! max number of ridging iterations
-
-      integer (kind=int_kind) :: &
-         n          , & ! thickness category index
-         niter      , & ! iteration counter
-         k          , & ! vertical index
-         it             ! tracer index
-
-      real (kind=dbl_kind) :: &
-         dti            ! 1 / dt
-
-      logical (kind=log_kind) :: &
-         iterate_ridging ! if true, repeat the ridging
-
-      character (len=char_len) :: &
-         fieldid        ! field identifier
 
 
 
