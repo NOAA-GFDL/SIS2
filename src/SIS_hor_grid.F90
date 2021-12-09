fcf54a42c103f2dd9be474e5132c1e05b375c0e8
Robert Hallberg <Robert.Hallberg@noaa.gov>
2021-12-09 13:50:59 -0500

    +Rescale G%Rad_Earth and set it via dG%Rad_Earth_L
      Rescaled the units of the Rad_Earth element of SIS_hor_grid_type and set it
    using the equivalently scale variable dG%Rad_Earth_L from the dyn_grid type.
    This change led naturally to changed units for the I_area_Earth internal
    variable in ice_diagnostics_init() and the cancellation of some grid rescaling
    factors that were there before.  This commit requires that SIS2 use a recent
    (November 27, 2021 or later) version of MOM6 from the dev/gfdl branch, and will
    not compile with earlier versions of the MOM6 code.  All answers are bitwise
    identical, but there is one minor interface change in a widely used transparent
    type.

--- src/SIS_hor_grid.F90
+++ src/SIS_hor_grid.F90
@@ -159,7 +159,7 @@ type, public :: SIS_hor_grid_type
   real :: west_lon      !< The longitude (or x-coordinate) of the first u-line
   real :: len_lat = 0.  !< The latitudinal (or y-coord) extent of physical domain
   real :: len_lon = 0.  !< The longitudinal (or x-coord) extent of physical domain
-  real :: Rad_Earth = 6.378e6 !< The radius of the planet [m].
+  real :: Rad_Earth     !< The radius of the planet [L ~> m], by default 6.378e6 m
   real :: max_depth     !< The maximum depth of the ocean [m].
 end type SIS_hor_grid_type
 
