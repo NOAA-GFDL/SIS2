1bf78a2868612e53e2b536f58e25c65224a32355
William-gregory <william.gregory@outlook.com>
2026-01-09 13:24:24 -0500

    add units flag

--- src/SIS_ctrl_types.F90
+++ src/SIS_ctrl_types.F90
@@ -241,11 +241,11 @@ subroutine ice_diagnostics_init(IOF, OSS, FIA, G, US, IG, diag, Time, Cgrid)
   
   !CMOR evaporation diagnostics
   FIA%id_evap_i   = register_SIS_diag_field('ice_model', 'sidmassevapsubl',diag%axesT1, Time, &
-               'Sea-Ice Mass Change Through Evaporation and Sublimation', 'kg m-2 s-1', &
+               'Sea-Ice Mass Change Through Evaporation and Sublimation', units='kg m-2 s-1', &
                conversion=US%RZ_T_to_kg_m2s, missing_value=missing, &
                cmor_standard_name='water_evapotranspiration_flux')
   FIA%id_evap_s   = register_SIS_diag_field('ice_model', 'sisndmasssubl',diag%axesT1, Time, &
-               'Snow Mass Rate of Change Through Evaporation or Sublimation', 'kg m-2 s-1', &
+               'Snow Mass Rate of Change Through Evaporation or Sublimation', units='kg m-2 s-1', &
                conversion=US%RZ_T_to_kg_m2s, missing_value=missing, &
                cmor_standard_name='tendency_of_atmosphere_mass_content_of_water_vapor_due_to_sublimation_of_surface_snow_and_ice')
               
@@ -351,7 +351,7 @@ subroutine ice_diagnostics_init(IOF, OSS, FIA, G, US, IG, diag, Time, Cgrid)
   
   !CMOR frazil diagnostic
   OSS%id_frazilmass   = register_SIS_diag_field('ice_model', 'sidmassgrowthwat', diag%axesT1, Time, &
-               'Sea-Ice Mass Change Through Growth in Supercooled Open Water (Frazil)', 'kg m-2 s-1', & 
+               'Sea-Ice Mass Change Through Growth in Supercooled Open Water (Frazil)', units='kg m-2 s-1', & 
                conversion=US%RZ_to_kg_m2, missing_value=missing, & 
                cmor_standard_name='tendency_of_sea_ice_amount_due_to_frazil_ice_accumulation_in_leads')
 
